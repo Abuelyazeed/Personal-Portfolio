191375527fda47f8ef1792d7136ac06d3740423b
Abuelyazeed <abuelyazeed175@gmail.com>
2024-07-02 21:31:46 +0300

    created and styled footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
@@ -11,6 +12,7 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </>
   );
 }

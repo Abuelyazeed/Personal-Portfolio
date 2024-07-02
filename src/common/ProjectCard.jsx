f7736bfcb2245fd5eae51d8db74210291c5bd727
Abuelyazeed <abuelyazeed175@gmail.com>
2024-07-02 20:59:52 +0300

    finished styling the projects section

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 function ProjectCard({ src, link, h3, p }) {
   return (
-    <div>
-      <a href={link}>
-        <img src={src} alt={`${h3} logo`} className="hover" />
-        <h3>{h3}</h3>
-        <p>{p}</p>
-      </a>
-    </div>
+    <a href={link} target="_blank">
+      <img src={src} alt={`${h3} logo`} className="hover" />
+      <h3>{h3}</h3>
+      <p>{p}</p>
+    </a>
   );
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,38 @@
 import styles from './Projects.module.css';
 import viberr from '../assets/viberr.png';
+import freshBurger from '../assets/fresh-burger.png';
+import hipsster from '../assets/hipsster.png';
+import fitLift from '../assets/fitlift.png';
 import ProjectCard from '../common/ProjectCard';
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="section-title">Projects</h1>
-      <div className={styles.projectContainer}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
         <ProjectCard
           src={viberr}
           link="https://to-do-app-cb459.web.app/"
           h3="To Do App"
           p="arr"
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Fresh Burger"
+          p="Hamburger Restaurant"
+        />
+        <ProjectCard
+          src={hipsster}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Hipsster"
+          p="Glasses Shop"
+        />
+        <ProjectCard
+          src={fitLift}
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="FitLift"
+          p="Fitness App"
         />
       </div>
     </section>
